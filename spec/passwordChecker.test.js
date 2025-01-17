a4c9237c68642478e064340641d6b185a8ff3547
Arwa Sheikh Idries <arwa.sheikh@student.vinci.be>
2025-01-17 16:56:05 +0100

    Add PasswordChecker class and initial test for minimum character requirement

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -3,3 +3,17 @@ describe("demo", function() {
         expect(1).toBe(1);
     });
 });
+
+import { PasswordChecker } from "../src/passwordChecker.js";
+
+describe("PasswordChecker", function () {
+   
+    it("should return true if the password has at least 8 characters", () => {
+        const password = "password8characters";
+    
+        const result = new PasswordChecker().minimum8Characters(password);
+    
+        expect(result).toBe(true);
+    });    
+    
+});
---
+++ src/passwordChecker.js
@@ -0,0 +1,10 @@
+export class PasswordChecker {
+
+    minimum8Characters(password) {
+        if (password.length >= 8) {
+            return true;
+        }
+    }
+
+
+}
